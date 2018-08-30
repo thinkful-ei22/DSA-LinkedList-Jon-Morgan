5cf9d5716292253f84f737bfa0b80ef43c9f7ac4
MorganHuegel <mhue2910@gmail.com>
2018-08-30 19:42:51 -0400

    updated doubly linked list

--- doubleLinkedList.js
+++ doubleLinkedList.js
@@ -30,8 +30,6 @@ class doubleLinkedList{
 
   }
 
-
-  // A -> B -> C -> D -> E -> null
   insertLast(value){
     if(this.head === null){
       const newNode = new _Node(value, null, null);
@@ -43,15 +41,86 @@ class doubleLinkedList{
       this.tail = newNode;
     }
   }
-};
+
+  insertBefore(before, item){
+    if(this.head === null){ //if the list is empty
+      const newNode = new _Node(item, null, null);
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (before === this.head.value){ //inserting before the first item
+      const newNode = new _Node(item, null, this.head);
+      this.head.prev = newNode;
+      this.head = newNode;
+    } else {
+      let currNode = this.head.next;
+      while (currNode !== null){
+        if(currNode.value === before){
+          const newNode = new _Node(item, currNode.prev, currNode);
+          currNode.prev = newNode;
+          newNode.prev.next = newNode;
+          break;
+        }
+        currNode = currNode.next;
+      }
+      if(currNode === null){
+        console.log('could not find item');
+        return;
+      }
+    }
+  }
+
+  insertAfter(after, item){
+    if(this.head === null){ //if the list is empty
+      const newNode = new _Node(item, null, null);
+      this.head = newNode;
+      this.tail = newNode;
+    } else if (this.tail.value === after){
+      const newNode = new _Node(item, this.tail, null);
+      this.tail.next = newNode;
+      this.tail = newNode;
+    } else {
+      let currNode = this.tail;
+      while(currNode !== null){  //counts down each node
+        if(currNode.value === after){
+
+          break;
+        }
+        currNode = currNode.prev;
+      }
+    }
+  }
+}
 
 
 function mainDLL(){
   const DLL = new doubleLinkedList();
-  
-  DLL.insertLast('hello');
-  DLL.insertLast('goodbye');
-  console.log(DLL);
+  DLL.insertFirst('C');
+  DLL.insertLast('D');
+  DLL.insertFirst('B');
+  DLL.insertFirst('A');
+  DLL.insertLast('E');
+  //DLL.insertBefore('A', '?');
+  //DLL.insertLast('hello');
+  //DLL.insertLast('goodbye');
+  //console.log(DLL);
+
+  /* ---------------- CIRCULAR STRUCTURE? ------------- */
+  //DLL LIST:   'A' <-> 'B' <-> 'C' <-> 'D' <-> 'E'
+  console.log(JSON.stringify(DLL, null, 2));
+  //circular because:  
+  /*
+  A.next: {
+    B.prev: {
+      A.next: {
+        B.prev: {
+          A.next: {
+            B.prev.......
+          }
+        }
+      }
+    }
+  }
+  */
 }
 
 mainDLL();
